540611e6875d7d28d4e99b33a4405d4b94dad332
afnx <afndeveloper@gmail.com>
2025-09-17 01:51:51 -0700

    feat: add logging utility for the LifeFinder project

--- lifefinder/utils/logger.py
+++ lifefinder/utils/logger.py
@@ -1,10 +1,15 @@
+"""
+Logging utility for the LifeFinder project.
+"""
+
 import logging
 import sys
+
 
 def get_logger(name=__name__, level=logging.INFO):
     logging.basicConfig(
         level=level,
         format="%(asctime)s | %(levelname)8s | %(name)s | %(message)s",
-        handlers=[logging.StreamHandler(sys.stdout)]
+        handlers=[logging.StreamHandler(sys.stdout)],
     )
     return logging.getLogger(name)
